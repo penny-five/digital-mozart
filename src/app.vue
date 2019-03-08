6e8ebe924eabfaddfe0beaf370b4399844321423
Joonas Lehtonen <lehton83@gmail.com>
2019-03-08 23:31:04 +0200

    implement first draft of UI

--- src/app.vue
+++ src/app.vue
@@ -1,12 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <header class="header">
+      <div class="content">
+        <h1>Digital Mozart</h1>
+      </div>
+    </header>
+    <main class="main">
+      <img class="logo" src="@/assets/images/logo.svg" />
+      <div class="content">
+        <h2>Your browser is now a composer.</h2>
+        <section class="section">
+          <p><strong>Digital Mozart</strong> is a next-gen AI-powered composer.</p>
+          <p>
+            It generates beautiful, thought-provoking tunes using an image you have provided as its
+            source of inspiration. It is powered by Google&reg; state-of-the-art image recognition
+            algorithms, machine learning and modern browser API's. Our long-term vision is to make
+            human composers obsolete.
+          </p>
+          <p>Welcome to the new era of music.</p>
+        </section>
+        <drop-area class="drop-area" />
+      </div>
+    </main>
+    <footer class="footer">
+      <div class="content">
+        <p>Digital Mozart&trade; 2019</p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
+import DropArea from './components/drop-area';
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    DropArea
+  }
 };
 </script>
 
@@ -14,11 +45,40 @@ export default {
 @import './assets/styles/styles.scss';
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .content {
+    margin: auto;
+    padding: 0 19px;
+    max-width: 900px;
+  }
+
+  .header {
+    border-bottom: 1px solid $color-grey;
+  }
+
+  .main {
+    min-height: calc(100vh - /* header */ 88px - /* footer */ 102px);
+    padding: 80px 0;
+  }
+
+  .footer {
+    padding: 20px;
+    text-align: center;
+  }
+
+  .section {
+    text-align: center;
+  }
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    padding-left: 20px;
+    height: 240px;
+    opacity: 0.9;
+  }
+
+  .drop-area {
+    margin: 50px auto;
+  }
 }
 </style>
---
+++ src/components/drop-area.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="drop-area">
+    <div class="icon" v-html="$options.icon"></div>
+    <span class="instructions">Drop image file here to generate music</span>
+  </div>
+</template>
+
+<script>
+import icon from '@/assets/images/upload.svg?inline';
+
+export default {
+  icon
+};
+</script>
+
+<style lang="scss">
+.drop-area {
+  max-width: 500px;
+  padding: 50px 20px;
+  text-align: center;
+  border: 1px solid $color-grey;
+  border-radius: 10px;
+  background-color: transparentize($color-grey, 0.9);
+
+  .icon {
+    display: block;
+    margin: 0 auto 25px;
+    height: 50px;
+    opacity: 0.25;
+    color: $color-black;
+
+    > svg {
+      height: inherit;
+    }
+  }
+
+  .instructions {
+    font-weight: $font-weight-bold;
+  }
+}
+</style>
